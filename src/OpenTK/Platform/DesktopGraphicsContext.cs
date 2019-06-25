70e460a00a96e0216563f388c1de5d82cb7d6ca1
Alexandru Bagu <alexandru.bagu@gmail.com>
2019-06-25 20:08:41 +0300

    removed all opengl except opengl es 2.0 calls

--- src/OpenTK/Platform/DesktopGraphicsContext.cs
+++ src/OpenTK/Platform/DesktopGraphicsContext.cs
@@ -36,13 +36,9 @@ namespace OpenTK.Platform
             Stopwatch time = Stopwatch.StartNew();
 
             #if OPENGL
-            new OpenTK.Graphics.OpenGL.GL().LoadEntryPoints();
-            new OpenTK.Graphics.OpenGL4.GL().LoadEntryPoints();
             #endif
             #if OPENGLES
-            new OpenTK.Graphics.ES11.GL().LoadEntryPoints();
             new OpenTK.Graphics.ES20.GL().LoadEntryPoints();
-            new OpenTK.Graphics.ES30.GL().LoadEntryPoints();
             #endif
 
             Debug.Print("Bindings loaded in {0} ms.", time.Elapsed.TotalMilliseconds);
--- src/OpenTK/Platform/Dummy/DummyGLContext.cs
+++ src/OpenTK/Platform/Dummy/DummyGLContext.cs
@@ -82,13 +82,9 @@ namespace OpenTK.Platform.Dummy
         public override void LoadAll()
         {
             #if OPENGL
-            new OpenTK.Graphics.OpenGL.GL().LoadEntryPoints();
-            new OpenTK.Graphics.OpenGL4.GL().LoadEntryPoints();
             #endif
             #if OPENGLES
-            new OpenTK.Graphics.ES11.GL().LoadEntryPoints();
             new OpenTK.Graphics.ES20.GL().LoadEntryPoints();
-            new OpenTK.Graphics.ES30.GL().LoadEntryPoints();
             #endif
         }
 
